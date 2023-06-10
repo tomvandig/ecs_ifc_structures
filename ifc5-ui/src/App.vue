1df6626b11eb18e390d1a7d80fb05f58319e953b
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2023-06-10 20:57:52 +0200

    Clean up UI

--- ifc5-ui/src/App.vue
+++ ifc5-ui/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import { ARC_LEDGER, MEP_LEDGER } from './ledgers';
 </script>
